b612263bad5eca1a1bbc444e87cb9044eb464310
Brandon Sievert <35996571+BWSIEVERT@users.noreply.github.com>
2025-02-28 22:42:24 -0500

    updated dashboard
    Added user access for dashboard. Displayed name as welcome message.

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,12 +1,17 @@
 import NavBar from "./NavBar";
 import "../App.css";
 
+import { useUser } from "@clerk/clerk-react";
+
 function Dashboard() {
+  const { user } = useUser();
+  console.log(user);
+
   return (
     <>
       <NavBar />
       <div className="dashboard_container">
-        <h1>Hello Dashboard</h1>
+        <h1>Welcome back, {user?.firstName}!</h1>
       </div>
     </>
   );
