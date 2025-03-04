081ba33742fbe11e0ae1ada9f30e22dc709d66b3
Brandon Sievert <35996571+BWSIEVERT@users.noreply.github.com>
2025-03-04 18:22:16 -0500

    Bug Fix
    Fixed an issue where the background would scroll when the mobile navigation was open.

--- src/Components/HamburgerComponent.jsx
+++ src/Components/HamburgerComponent.jsx
@@ -1,10 +1,21 @@
 import { UserButton } from "@clerk/clerk-react";
 import "../Stylesheets/HamburgerComponentStyle.css";
 import Hamburger from "hamburger-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function HamburgerComponent() {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    // Disable scroll when the menu is open
+    document.body.style.overflow = open ? "hidden" : "auto";
+
+    // Cleanup function to ensure overflow is reset if the component unmounts
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
   return (
     <>
       <header className="header_container">
